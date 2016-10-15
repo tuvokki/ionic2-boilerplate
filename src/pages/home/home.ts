9c7eb223acfa2be805be4e5a040e204b43b6ca89
Wouter Roosendaal <wouter.roosendaal@planonsoftware.com>
2016-10-15 12:28:50 +0200

    feat(browser): Consume the websocket from elogic
    Subscribe to the websocket from the backend project called elogic running on localhost 3000

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+// import * as io from 'socket.io-client';
+import io from 'socket.io-client';
+
 
 @Component({
     selector: 'page-home',
@@ -7,8 +10,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
     public user;
+    public serverTime;
+    public message;
+    public socket = null;
+    public conf = {
+        connectionServer: 'http://localhost:3000/',
+    };
 
     constructor(public navCtrl: NavController) {
+        this.socket = io(this.conf.connectionServer);
+        this.socket.on('connect', () => {
+            console.log('Connected');
+        });
+        this.socket.on('time', (timeString) => {
+            this.serverTime = 'Server time: ' + timeString;
+        });
+        this.socket.on('message', (messageString) => {
+            this.message = messageString;
+        });
         // console.log(process.env.NODE_ENV);
         // const myArr = [
         //     {
@@ -24,10 +43,16 @@ export class HomePage {
         //
         // this.user = (_.filter(myArr, o => !o.active))[0];
         this.user = {
-                    name: 'barney',
-                    age: 36,
-                    active: true,
-                };
+            name: 'barney',
+            age: 36,
+            active: true,
+        };
     }
 
+    public init() {
+        console.log('test');
+        this.serverTime = 'Server time: init';
+        this.message = 'message init';
+
+    }
 }
